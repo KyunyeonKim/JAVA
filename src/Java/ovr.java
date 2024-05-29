dd358c07436e059bba8985bbeacbc42e17170058
KyunyeonKim <kgn4746@naver.com>
2024-05-29 22:24:02 +0900

    공부

---
+++ src/Java/ovr.java
@@ -0,0 +1,21 @@
+package Java;
+
+public class ovr {
+    public static void main(String[] args){
+        int a=1,b=2,c=3,d=4;
+
+        int mx,mn;
+
+        mx = a<b?b:a;
+
+        if(mx==1){
+            mn = a>mx?b:a;
+        }
+        else{
+            mn=b<mx?d:c;
+        }
+        System.out.println(mn);
+
+
+    }
+}
