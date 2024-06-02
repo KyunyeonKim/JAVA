ed5127dc16498a123fcbd099f0d692cb9a92fc93
KyunyeonKim <kgn4746@naver.com>
2024-06-02 17:15:47 +0900

    공부

---
+++ src/Java/AccurayExample.java
@@ -0,0 +1,12 @@
+package Java;
+
+public class AccurayExample {
+    public static void main(String[] args) {
+        int apple = 1;
+        double pieceUnit = 0.1;
+        int number = 7;
+
+        double result = apple - number*pieceUnit ;
+        System.out.println("사과 1개에서의 남은양" + result);
+    }
+}
