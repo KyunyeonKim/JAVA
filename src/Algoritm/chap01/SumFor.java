28571d55de33aa8da7c1bd59c817166300a876f8
KyunyeonKim <kgn4746@naver.com>
2024-08-04 21:07:11 +0900

    08/04 알고리즘

--- src/Algoritm/chap01/SumFor.java
+++ src/Algoritm/chap01/SumFor.java
@@ -1,6 +1,23 @@
 package Algoritm.chap01;
 
+import java.util.Scanner;
+
 public class SumFor {
 
-    int a=10;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("1부터 n까지의 합을 구합니다.");
+        System.out.println("n값: ");
+         int n = sc.nextInt();
+
+         int sum=0;
+
+         for(int i=1; i<=n; i++)
+             sum += i;
+
+        System.out.println("1부터 n까지의 합은:"+ sum);
+
+
+    }
 }
