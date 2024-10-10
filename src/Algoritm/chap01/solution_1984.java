616d0bcece9804fe02a93bf3194b3949509bd3c7
KyunyeonKim <kgn4746@naver.com>
2024-10-10 23:18:15 +0900

    10/10 알고리즘 문제

---
+++ src/Algoritm/chap01/solution_1984.java
@@ -0,0 +1,32 @@
+package Algoritm.chap01;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class solution_1984 {
+    public static void main(String args[]) throws Exception{
+        Scanner sc = new Scanner(System.in);
+        int T;
+        T = sc.nextInt();
+
+        for(int test_case = 1; test_case <= T; test_case++){
+            Float[] array = new Float[10];
+            for(int i=0; i<10; i++) {
+                array[i] = sc.nextFloat();
+            }
+
+            Arrays.sort(array);
+
+            float sum = 0f;
+            int result = 0;
+            for(int i=0; i < array.length; i++) {
+                if(i!=0 && i != array.length-1){
+                    sum += array[i];
+                }
+            }
+            result = Math.round(sum / (array.length-2));
+
+            System.out.println("#"+(test_case)+" "+result);
+        }
+    }
+}
