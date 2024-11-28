bac883d75f4642ea2c1fd1a9f5b2b34f7aa73dc5
KyunyeonKim <kgn4746@naver.com>
2024-11-28 18:02:25 +0900

    11/28 이것이 자바다

---
+++ src/ThisJava/ch02/ArrayCreateByValueListExample2.java
@@ -0,0 +1,11 @@
+package ThisJava.ch02;
+
+public class ArrayCreateByValueListExample2 {
+    public static void main(String[] args) {
+        //배열 변수 선언
+        int [] scores;
+
+        //배열 변수에 배열을 대입
+        scores = new int[]{83,90,87};
+    }
+}
